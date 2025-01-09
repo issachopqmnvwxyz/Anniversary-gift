b3a8402c5118a05d6dffef1290f48f981e53f0f3
issachopqmnvwxyz <isachnugroho001@gmail.com>
2025-01-09 21:35:23 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var x = document.createElement("CODE");
+    var t = document.createTextNode("I love youu so much yub🫶😋💞");
+    x.appendChild(t);
+    document.body.appendChild(x);
+  }
